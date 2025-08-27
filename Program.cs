8a5f03a565f9256339f2e1c73963d6886dac58c9
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-08-27 08:45:20 -0300

    Criação do arquivo C#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Screen Sound
+
+string mensagemDeBoasVindas = "Bem vindo ao Flavio Sound";
+
+void ExibirBoasVindas()
+{
+    Console.WriteLine(@"
+    
+███████╗██╗░░░░░░█████╗░██╗░░░██╗██╗░█████╗░  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██║░░░░░██╔══██╗██║░░░██║██║██╔══██╗  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+█████╗░░██║░░░░░███████║╚██╗░██╔╝██║██║░░██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+██╔══╝░░██║░░░░░██╔══██║░╚████╔╝░██║██║░░██║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██║░░░░░███████╗██║░░██║░░╚██╔╝░░██║╚█████╔╝  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 0 para sair");
+
+    Console.WriteLine("Digite a sua opção : ");
+    Console.ReadLine();
+}
+
+
+ExibirBoasVindas();
+ExibirOpcoesMenu();
